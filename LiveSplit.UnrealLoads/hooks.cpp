0bf791c8591dde232d96dceaf74b2c10d09020f8
Dalet <dmolet@outlook.com>
2018-01-02 09:22:22 +0100

    remove windows.h include

--- LiveSplit.UnrealLoads/hooks.cpp
+++ LiveSplit.UnrealLoads/hooks.cpp
@@ -1,10 +1,11 @@
-#include <windows.h>
-
 #define STATUS_NONE			0
 #define STATUS_LOADING_MAP	1
 #define STATUS_SAVING		2
 
 #define DllExport	__declspec(dllexport)
+
+#define MAX_PATH 260
+typedef char16_t utf16_char;
 
 typedef void *(__thiscall *t_LoadMap)(void*, const void*, void*, const void*, void*);
 typedef void *(__thiscall *t_LoadMap_oldUnreal)(void*, const void*, void*, const void*, void*, void*);
@@ -24,8 +25,8 @@ t_SaveGame_SplinterCell		g_oSaveGame_SplinterCell;
 t_SaveGame_SplinterCell3	g_oSaveGame_SplinterCell3;
 t_saveGame_DeusEx			g_oSaveGame_DeusEx;
 
-DllExport int		g_status = STATUS_NONE;
-DllExport wchar_t	g_map[MAX_PATH];
+DllExport int			g_status = STATUS_NONE;
+DllExport utf16_char	g_map[MAX_PATH];
 
 DllExport
 void set_map_ascii(const char *map)
@@ -36,27 +37,24 @@ void set_map_ascii(const char *map)
 		if (map[i] == '\0')
 			break;
 	}
-	g_map[MAX_PATH - 1] = '\0';
 }
 
 DllExport
-void set_map_utf16(const wchar_t *map)
+void set_map_utf16(const utf16_char *map)
 {
 	for (int i = 0; i < MAX_PATH; i++)
 	{
 		g_map[i] = map[i];
-		if (map[i] == L'\0')
+		if (map[i] == 0)
 			break;
 	}
-	g_map[MAX_PATH - 1] = L'\0';
 }
-
 
 DllExport
 void* __fastcall Detour_LoadMap(void *This, void *edx, const void *URL, void *Pending, const void *TravelInfo, void *Error)
 {
-	wchar_t *map = *((wchar_t **)URL + 7);
-	set_map(map);
+	utf16_char *map = *((utf16_char **)URL + 7);
+	set_map_utf16(map);
 
 	g_status = STATUS_LOADING_MAP;
 	void *level = g_oLoadMap(This, URL, Pending, TravelInfo, Error);
@@ -68,8 +66,8 @@ void* __fastcall Detour_LoadMap(void *This, void *edx, const void *URL, void *Pe
 DllExport
 void* __fastcall Detour_LoadMap_oldUnreal(void *This, void *edx, const void *URL, void *Pending, const void *TravelInfo, void *Error, void *UTravelDataManager)
 {
-	wchar_t *map = *((wchar_t **)URL + 7);
-	set_map(map);
+	utf16_char *map = *((utf16_char **)URL + 7);
+	set_map_utf16(map);
 
 	g_status = STATUS_LOADING_MAP;
 	void *level = g_oLoadMap_oldUnreal(This, URL, Pending, TravelInfo, Error, UTravelDataManager);
@@ -81,8 +79,8 @@ void* __fastcall Detour_LoadMap_oldUnreal(void *This, void *edx, const void *URL
 DllExport
 void* __fastcall Detour_LoadMap_SplinterCell(void *This, void *edx, const void *URL, void *Error)
 {
-	wchar_t *map = *((wchar_t **)URL + 7);
-	set_map(map);
+	utf16_char *map = *((utf16_char **)URL + 7);
+	set_map_utf16(map);
 
 	g_status = STATUS_LOADING_MAP;
 	void *level = g_oLoadMap_SplinterCell(This, URL, Error);
@@ -94,8 +92,8 @@ void* __fastcall Detour_LoadMap_SplinterCell(void *This, void *edx, const void *
 DllExport
 void __fastcall Detour_LoadMap_SplinterCell3(void *This, void *edx, const void *URL, void *Error)
 {
-	wchar_t *map = *((wchar_t **)URL + 7);
-	set_map(map);
+	utf16_char *map = *((utf16_char **)URL + 7);
+	set_map_utf16(map);
 
 	g_status = STATUS_LOADING_MAP;
 	g_oLoadMap_SplinterCell3(This, URL, Error);
