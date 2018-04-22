dcb7328665ff51e1a0c7d3098d66af3beb14f794
Dalet <dmolet@outlook.com>
2018-04-22 02:46:47 +0200

    update hp3 maps

--- LiveSplit.UnrealLoads/Games/HarryPotter3.cs
+++ LiveSplit.UnrealLoads/Games/HarryPotter3.cs
@@ -19,18 +19,37 @@ namespace LiveSplit.UnrealLoads.Games
 
 		public override HashSet<string> Maps => new HashSet<string>
 		{
+			"hp3_adv1express",
 			"hp3_ch1carperetractum",
 			"hp3_ch1carperetractumb",
+			"hp3_insidehub",
 			"hp3_groundshub",
 			"hp3_comc",
+			"hp3_ch2draconifors",
 			"hp3_ch2draconiforsb",
+			"hp3_quidditch",
 			"hp3_adv2exppatronum",
-			"hp3_adv2glaciusb",
-			"hp3_adv4shack",
+			"hp3_adv3library",
+			"hp3_ch3glacius",
+			"hp3_ch3glaciusb",
+			"hp3_buckyexecuted",
+			"hp3_adv5shack",
 			"hp3_whompingwillow",
-			"hp3_infirmary",
 			"hp3_dementorbattle",
-			"hp3_fa1ron"
+			"hp3_infirmary",
+			"hp3_adv6paddock",
+			"hp3_darktower",
+			"hp3_fa1ron",
+			"hp3_fa2hermione",
+			"hp3_fa3harry",
+			"hp3_groundsdada",
+			"hp3_portraitpassword_b",
+			"hp3_portraitpassword_c",
+			"hp3_portraitpassword_d",
+			"hp3_portraitpassword_e",
+			"hp3_portraitpassword_f",
+			"hp3_dungeonhub",
+			"hp3_beanbonus"
 		};
 	}
 }
